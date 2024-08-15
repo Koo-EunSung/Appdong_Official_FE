81260b138d1ad535c128cd97b65282b51094e886
Koo-EunSung <ku903@naver.com>
2024-08-15 20:58:35 +0900

    Design: 슬라이드 메뉴 배경색 변경

--- src/components/Header.js
+++ src/components/Header.js
@@ -39,7 +39,7 @@ function Header() {
                 </nav>
             </div>
             {/* 슬라이드 메뉴 */}
-            <div className={`fixed top-0 left-0 w-64 bg-sky-500 h-full z-20 transform ${isOpen ? "translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out md:hidden`}>
+            <div className={`fixed top-0 left-0 w-64 bg-sky-600 h-full z-20 transform ${isOpen ? "translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out md:hidden`}>
                 {/* 닫기 아이콘 */}
                 <div className="flex justify-between items-center p-4">
                     <div>
