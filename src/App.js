f8cda0456b5285dae818adbcc57abd384b5875e9
Koo-EunSung <ku903@naver.com>
2024-09-02 23:45:55 +0900

    Feat: 라우팅 추가 및 가입신청서 목록 조회 화면 구현

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import './App.css';
-import Header from './components/Header';
-import logo_transparent from './logo_transparent.png';
-
 import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from './page/Main';
+import FormList from './page/FormList';
 
 function App() {
-  const [headerHeight, setHeaderHeight] = useState(0);
-
-  useEffect(() => {
-    const header = document.querySelector('header');
-    setHeaderHeight(header.offsetHeight);
-  }, []);
-
   return (
-    <div className="App bg-sky-100 h-dvh">
-      <Header />
-      <main className="flex flex-col justify-center items-center py-36" style={{marginTop: `${headerHeight}px`}}>
-        <div className="flex flex-col justify-center items-center">
-          <img className="w-10/12 md:w-full" src={logo_transparent}/>
-        </div>
-        <h1 className='text-2xl font-bold'>경북대 중앙동아리 유일 앱개발 동아리</h1>
-        <h2 className='text-xl font-semibold'>앱동 공식 홈페이지에 오신 것을 환영합니다</h2>
-        <button className='bg-sky-500 text-white rounded-xl mt-3 px-5 py-3 transition ease-in-out delay-75 hover:bg-blue-600 duration-200'>가입 신청</button>
-      </main>
-    </div>
+    <Routes>
+      <Route path="/" element={<Main />}/>
+      <Route path="/register" element={<FormList />}/> 
+    </Routes>
   );
 }
 
---
+++ src/page/FormList.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import Header from "../components/Header"
+import axios from "../api/customAxios";
+
+function FormList() {
+    const [headerHeight, setHeaderHeight] = useState(0);
+    const [form, setForm] = useState([
+    ]);
+
+    useEffect(() => {
+      const header = document.querySelector('header');
+      setHeaderHeight(header.offsetHeight);
+/*
+      axios.get('/form')
+        .then((res) => {
+            console.log(res.data);
+            setForm(res.data);
+        })
+        .catch((err) => console.log(err.message));
+*/
+    }, []);
+
+    const getForm = form.map((item) => (
+        <div className="flex flex-col justify-center w-3/4 md:w-3/5 rounded-md bg-white mb-5">
+            <h2 className="px-5 py-4 text-3xl text-white bg-blue-500 rounded-t-md">{item.title}</h2>
+            <p className="px-5 py-5 text-xl whitespace-pre-line">{item.description}</p>
+            <button className='bg-blue-500 text-white text-xl rounded-xl ml-auto mx-5 my-5 px-5 py-3 w-fit align- transition ease-in-out delay-75 hover:bg-sky-400 duration-100'>작성</button>
+        </div>
+    ))
+
+    return (
+        <div className="bg-sky-100 h-full">
+            <Header />
+            <main className="flex flex-col" style={{marginTop: `${headerHeight}px`}}>
+                <div className="flex flex-col justify-center items-center text-center py-10 bg-black bg-opacity-25">
+                    <h1 className="text-white text-4xl font-bold">가입 신청</h1>
+                    <p className="text-white text-xl mt-2 w-2/4">앱동의 일원으로서 함께 변화하고 성장할 여러분을 기다립니다</p>
+                </div>
+                <div className="flex flex-col justify-center items-center mt-10">
+                    {
+                        form.length === 0
+                        ? 
+                        <div className="text-center text-black w-3/4 md:w-3/5 py-24 h-dvh">
+                            <p className="text-2xl font-bold">현재 가입 신청 기간이 아닙니다</p>
+                            <p className="text-xl">오류라고 생각되시면, 동아리 공식 인스타를 통해 문의해주세요</p>
+                            <p className="text-lg">공식 인스타그램: @knu.appdong</p>
+                        </div>
+                        : getForm
+                    }
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default FormList;
