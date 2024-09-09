6ca3db2b4d9a4541de3abddd530a807c944430a4
Koo-EunSung <ku903@naver.com>
2024-09-09 14:53:34 +0900

    Feat: 설문지 답변 조회 페이지

--- src/page/FormResult.js
+++ src/page/FormResult.js
@@ -28,7 +28,7 @@ function FormResult() {
     return (
         <div>
             <h1>설문지 답변 조회 페이지</h1>
-            {form != undefined && form.length != 0 && 
+            {form !== undefined && form.length !== 0 && 
             <select
                 onChange={(event) => setCurrentForm(event.target.value)}>
                 <option disabled hidden selected value="">조회할 폼을 선택하세요</option>
@@ -38,22 +38,24 @@ function FormResult() {
             </select>
             }
             <div>
-                {data != undefined && data.length != 0 && (
+                {data !== undefined && data.length !== 0 && (
                     <table className="border-collapse">
                         <tr>
+                            <th className="border border-black px-5 py-3 align-middle"scope="col">제출 시각</th>
                             <th className="border border-black px-5 py-3 align-middle"scope="col">이름</th>
                             <th className="border border-black px-5 py-3 align-middle" scope="col">학번</th>
                             {data?.question.map((question) => (
-                                <th className="border border-black px-5 py-3 align-middle" scope="col">{question.title}</th>
+                                <th className="border border-black px-5 py-3 align-middle whitespace-nowrap" scope="col">{question.title}</th>
                             ))}
                         </tr>
                         {data.answerSheet.length !== 0 && (
                             data.answerSheet.map((answer) => (
                                 <tr>
+                                    <td className="border border-black px-5 py-3 allign-midde">{answer.createdAt}</td>
                                     <td className="border border-black px-5 py-3 align-middle">{answer.name}</td>
                                     <td className="border border-black px-5 py-3 align-middle">{answer.studentId}</td>
-                                    {answer.answers.map((ans) => (
-                                        <td className="border border-black px-5 py-3 align-middle whitespace-pre">{ans.answer}</td>
+                                    {data.question.map((question) => (
+                                        <td className="border border-black px-5 py-3 align-middle whitespace-pre max-w-screen-lg overflow-auto">{answer.answers.find((ans) => ans.questionId === question.id).answer}</td>
                                     ))}
                                 </tr>
                             ))
